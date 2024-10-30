cd2e85f471d5459c88644a63097e18a4ed3d17fa
Mamtesh2001 <89520587+Mamtesh2001@users.noreply.github.com>
2024-10-30 22:32:01 +0530

    Create seed.sql

---
+++ Phase-1 Project!!/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO clients (name, contact) VALUES ('John Doe', 'john@example.com');
+INSERT INTO clients (name, contact) VALUES ('Jane Smith', 'jane@example.com');
+
+INSERT INTO meetings (client_id, date) VALUES (1, '2024-11-01 10:00:00');
+INSERT INTO meetings (client_id, date) VALUES (2, '2024-11-02 14:00:00');
